97b7e66542f0ca3906b3b3a60310f5d77e91ef33
whoishannahz <whoishannah.z@gmail.com>
2018-11-02 14:56:37 +0000

    the roman-numeral works! everything is good

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -1,18 +1,36 @@
 class RomanNumerals
+  @@arabic_to_roman = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
 
   def calcul(number)
-      dictionary = {
-        1 => 'I',
-        5 => 'V',
-        10 => 'X',
-        50 => 'L',
-        100 => 'C',
-        500 => 'D',
-        1000 => 'M'
-      }
-      dictionary.has_key?(number)
-      value = dictionary[number]
-      p "this is the value #{number} #{value}"
-      return dictionary[number]
-    end
+    bucket = []
+    #verify all every element of my array and compare it to my number.
+    #if my number is >= to the key then write the value and put it in the bucket and number = number - value
+    #then if its not matching, please continue to inspect the other element of my array @@arabic_to_roman
+    @@arabic_to_roman.each do |key, value|
+      while number >= key
+        bucket << value
+        number = number - key
+      end
+  end
+      p "do i return this? #{bucket.join}"
+      return bucket.join
 end
+end
+    #if my number is >= to the key then continu to verify the element of the array until the number match
+    #then return it as a string
+  #  number == @@arabic_to_roman[number]
+    #  @@arabic_to_roman.has_key?(number)
--- spec/roman-numerals_spec.rb
+++ spec/roman-numerals_spec.rb
@@ -9,6 +9,18 @@ describe RomanNumerals do
     roman_numerals = RomanNumerals.new
     expect(roman_numerals.calcul(5)).to eq 'V'
   end
+  it 'returns "II" when I have "2"' do
+    roman_numerals = RomanNumerals.new
+    expect(roman_numerals.calcul(2)).to eq 'II'
+  end
+  it 'returns "IX" when I have "9"' do
+    roman_numerals = RomanNumerals.new
+    expect(roman_numerals.calcul(9)).to eq 'IX'
+  end
+  it 'returns "XLIV" when I have "44"' do
+    roman_numerals = RomanNumerals.new
+    expect(roman_numerals.calcul(44)).to eq 'XLIV'
+  end
 end
 # describe "unite" do
 #   it 'returns "II" when I have "2"' do
