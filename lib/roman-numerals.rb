badfae8d73bd1cd2c80dbf3d109fcc5b72a179a0
whoishannahz <whoishannah.z@gmail.com>
2018-11-02 10:50:55 +0000

    starting the dictionnary

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -1,5 +1,21 @@
-def roman_numerals(number)
-  if number == 1
-    return "I"
-  end
+class RomanNumerals
+
+  def calcul(number)
+    if number == 1
+      return "I"
+    # elsif number == 2
+    #   return "I" + "I"
+    end
+    # def convert()
+    #   dictionary = {
+    #     1 => 'I'
+    #     5 => 'V'
+    #     10 => 'X'
+    #     50 => 'L'
+    #     100 => 'C'
+    #     500 => 'D'
+    #     1000 => 'M'
+    #   }
+    # end
+end
 end
--- spec/roman-numerals_spec.rb
+++ spec/roman-numerals_spec.rb
@@ -1,6 +1,19 @@
 require 'roman-numerals'
-describe "roman-numerals" do
-  it 'returns "I" when I have "1"' do
-    expect(roman_numerals(1)).to eq 'I'
+
+  describe RomanNumerals do
+    it 'returns "I" when I have "1"' do
+      roman_numerals = RomanNumerals.new
+    expect(roman_numerals.calcul(1)).to eq 'I'
+    end
   end
-end
+# describe "unite" do
+#   it 'returns "II" when I have "2"' do
+#   expect(unite(2)).to eq 'II'
+#   end
+# end
+#   describe "unite" do
+#     it 'returns a new symbole "V" when I have "5"' do
+#     expect(unite(2)).to eq 'II'
+#   end
+  # end
+# end
